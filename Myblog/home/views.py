3d1886f443f4596ba0689a0f369972439bcbdd3a
Ravi kumar <cyboravidell@gmail.com>
2023-03-21 03:37:54 +0530

    Sign In and Sign Up with backend completed

--- Myblog/home/views.py
+++ Myblog/home/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render,redirect, HttpResponse
 from home.models import Contact
 from Blog.models import Post
 from django.contrib import messages
+from django.contrib.auth.models import User
+
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 def home(request):
     return render(request,"home/home.html")
@@ -46,3 +49,57 @@ def search(request):
     params = {'allposts' : allposts,
               'query': query}
     return render(request, 'home/search.html', params)
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['inputfname']
+        lname = request.POST['inputlname']
+        email = request.POST['inputemail']
+        pass1 = request.POST['inputPassword1']
+        pass2 = request.POST['inputPassword2']
+
+        # check for errorneous input
+        if len(username)>10:
+
+            if username.isdigit():
+                messages.error(request, "Your username must contain atleast 1 character")
+
+            messages.error(request, "Your username must be less than 10 characters")
+            return redirect('home')
+        
+        if pass1 != pass2:
+            messages.error(request,"Password do not match, Please check the password and try again")
+            return redirect('home')
+        
+        # Creating User
+        else:
+            myuser = User.objects.create_user(username,email,pass1)
+            myuser.first_name = fname
+            myuser.last_name = lname
+            myuser.save()
+            messages.success(request,"Your account has been created Successfully")
+        return redirect('home')
+    
+def blogsignin(request):
+    if request.method == "POST":
+
+        loginusername = request.POST['loginusername']
+        loginpassword = request.POST['loginpassword']
+
+        user = authenticate(username = loginusername, password = loginpassword)
+        if user is None:
+            messages.error(request, "Invalid Credentials, Please check and retry again")
+            return redirect('home')
+        else:
+            login(request,user)
+            messages.success(request, "Successfully Logged In")
+            return redirect('home')
+    else:
+        HttpResponse("404 - Not Found")
+
+
+def bloglogout(request):
+    logout(request)
+    messages.success(request, "Successfully Logout")
+    return redirect('home')
