ed301fdb832007a8d6a817c8553cb0b5825b3d7e
Ravi kumar <cyboravidell@gmail.com>
2023-03-21 04:54:53 +0530

    Blog Comment database model has been created

--- Myblog/Blog/models.py
+++ Myblog/Blog/models.py
@@ -1,4 +1,7 @@
+from pickle import TRUE
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 class Post(models.Model):
     Sno = models.AutoField(primary_key=True)
@@ -10,3 +13,12 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return "Posted by "+ self.author + " - " + self.title
+
+class BlogComment(models.Model):
+    sno = models.AutoField(primary_key=True)
+    comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=TRUE)
+    timestamp = models.DateTimeField(default=now)
+    
--- Myblog/Myblog/urls.py
+++ Myblog/Myblog/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+
+admin.site.site_header = "MyBlog Admin"
+admin.site.site_title = "MyBlog Admin Panel"
+admin.site.index_title = "Welcome to MyBlog Admin Panel"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
